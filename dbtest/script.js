bbe0fcdbcd0654d2bc9492a6dd068a726e60db0b
Shun611 <shun31425@gmail.com>
2021-05-31 12:19:56 +0900

    DBtestタイマー表示変更

--- dbtest/script.js
+++ dbtest/script.js
@@ -29,30 +29,40 @@ window.addEventListener("load", function(){
 time();
 
 function time() {
-    if(bus_type == "3"){
-      document.querySelector("#timer").innerHTML = "運休です";
-    }
-    else if(bus_type == "4"){
-      document.querySelector("#timer").innerHTML = "特別ダイヤです";
-    }
+  if(bus_type == "3"){
+    document.querySelector("#timer").innerHTML = "運休です";
+  }
+  else if(bus_type == "4"){
+    document.querySelector("#timer").innerHTML = "特別ダイヤです";
+  }
 
-    if(bus_time != null){
-      var bus = bus_time.split(':');
-      var now = new Date();
-      var hours = now.getHours();
-      var minutes = now.getMinutes();
-      var seconds = now.getSeconds();
-      hours = Number(bus[0]) - hours;
-      if(hours <= 9) hours = "0" + hours;
-      minutes = Number(bus[1]) - minutes - 1;
-      if(minutes <= 9) minutes = "0" + minutes;
-      seconds = 60 - seconds;
-      if(seconds == 60) seconds = "00";
+  if(bus_time != null){
+    var bus = bus_time.split(':');
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    hours = Number(bus[0]) - hours;
+    minutes = Number(bus[1]) - minutes - 1;
+    if(minutes < 0){
+      hours = hours - 1;
+      minutes = minutes + 60;
+    }
+    seconds = 60 - seconds;
+    if(hours < 0 || minutes < 0 || seconds < 0) updatedata();
+    else{
+      if(0 <= hours && hours <= 9) hours = "0" + hours;
+      if(seconds == 60){
+        seconds = "00";
+        minutes = minutes + 1;
+      }
       else if(seconds <= 9) seconds = "0" + seconds;
+      if(minutes <= 9) minutes = "0" + minutes;
 
       var countdown = hours + ':' + minutes + ':' + seconds;
       document.querySelector("#timer").innerHTML = countdown;
     }
+  }
 }
 
 setInterval('time()', 1000);
