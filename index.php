e0fcc58e3904f9a424df8ca4643b6c9b806556e4
shuto0410 <69708307+shuto0410@users.noreply.github.com>
2021-06-27 12:24:27 +0900

    途中

--- index.php
+++ index.php
@@ -206,6 +206,7 @@
     </div>
     <script src="js/script.js"></script>
     <script src="js/script2.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </body>
 
 </html>
---
+++ js/train_time_table.js
@@ -0,0 +1,65 @@
+// var data;
+// function get_schedule() {
+//     var request = new XMLHttpRequest();
+//     request.open("POST", "dbtest/bus_schedule.php", true);
+//     request.responseType = 'json';
+//     request.setRequestHeader("content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+//     request.send('select_station=' + encodeURIComponent(document.getElementById("bus_timer_select").value));
+
+//     request.onreadystatechange = function () {
+//         if (request.readyState == 4 && request.status == 200) {
+//             data = request.response;
+//             console.log(data);
+//             if (data !== null) {
+//                 clean_table();
+//                 write_schedule();
+//             }
+//         }
+//     }
+// }
+
+// function clean_table() {
+//     var table = document.querySelector(".bus_schedule_table");
+//     for (let i = 0; i < table.rows.length; i++) {
+//         table.rows[i].cells[1].innerText = "";
+//     }
+// }
+
+// function write_schedule() {
+//     var obj = document.getElementById("bus_timer_select");
+//     var idx = obj.selectedIndex;
+//     document.querySelector(".destination").innerHTML = obj.options[idx].text;
+//     var table = document.querySelector(".bus_schedule_table");
+//     var hours_tmp = table.rows;
+//     var hours = [];
+//     // htmlないから08~22を抜いている\tが邪魔だから抜いている
+//     for (let i = 0; i < hours_tmp.length; i++) {
+//         hours[i] = hours_tmp[i].innerText.replace("\t", "");
+//     }
+
+//     for (let i = 0; i < data.length - 1; i++) {
+//         var time = data[i].split(':');
+//         var index = hours.indexOf(time[0]);
+//         table.rows[index].cells[1].insertAdjacentHTML('beforeend', time[1] + "&emsp;");
+//     }
+// }
+$(function(){
+    $('#sel').on("change",function(){
+        $.post({
+            url: 'ajax_getData.php',
+            data:{
+                'opt': 'hoge'
+            },
+            dataType: 'json', //必須。json形式で返すように設定
+        }).done(function(data){
+            alert(ok);
+        }).fail(function(XMLHttpRequest, textStatus, errorThrown){
+            alert(errorThrown);
+        })
+    })
+})
+
+// window.addEventListener("load", get_schedule());
+
+// const select = document.getElementById("bus_timer_select");
+// select.addEventListener('change', get_schedule);
